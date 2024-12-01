89fc2be371ab5d6a3b70523743fb8211bed33434
felipeBarras <felipeu_santos@hotmail.com>
2024-12-01 19:19:59 -0300

    configurando a criação da função Lambda e adicionando a função criada ao fluxo

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // criação de um index extremamente básico somente para boas práticas
-const express = require('express');
-const app = express();
-const routes = require('./routes');
+// const express = require('express');
+// const app = express();
+// const routes = require('./routes');
 
-require('dotenv').config(); // uso para arquivo ENV, se necessário
+// require('dotenv').config(); // uso para arquivo ENV, se necessário
 
-app.use(express.json());
-app.use('/', routes);
+// app.use(express.json());
+// app.use('/', routes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+exports.handler = async (event) => {
+  console.log(event);
+  return 'Hello from Lambda!';
+};
