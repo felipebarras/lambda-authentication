52e675c5c90e77d443c547df5aab18d074862f0e
felipeBarras <felipeu_santos@hotmail.com>
2024-12-09 14:55:15 -0300

    fix: ajeitando políticas dentro do iam-role.tf

--- iam-role.tf
+++ iam-role.tf
@@ -1,22 +1,40 @@
-# # Role para o Lambda
-# resource "aws_iam_role" "lambda_exec_role" {
-#   name = "lambda_exec_role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Action    = "sts:AssumeRole",
-#         Effect    = "Allow",
-#         Principal = {
-#           Service = "lambda.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+# Role para o Lambda
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_policy_attachment" "lambda_basic_execution" {
-#   name       = "lambda_basic_execution"
-#   roles      = [aws_iam_role.lambda_exec_role.name]
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
+resource "aws_iam_policy_attachment" "lambda_basic_execution" {
+  name       = "lambda_basic_execution"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy_attachment" "gw_fiap_policy" {
+  name       = "gw-fiap-policy"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = "arn:aws:iam::180294201352:policy/gw-fiap-policy"
+}
+
+resource "aws_iam_policy_attachment" "iam_fiap_policy" {
+  name       = "iam-fiap-policy"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = "arn:aws:iam::180294201352:policy/iam-fiap-policy"
+}
+
+resource "aws_iam_policy_attachment" "lambda_fiap_policy" {
+  name       = "lambda-fiap-policy"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = "arn:aws:iam::180294201352:policy/lambda-fiap-policy"
+}
